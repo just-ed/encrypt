0be6e536ca85c5463594914c318d0f7e218a50e7
Edgar Tomeyan <ed_tomeyan@mail.ru>
2019-01-27 18:28:42 +0300

    modified case statement

--- encrypt.rb
+++ encrypt.rb
@@ -20,8 +20,14 @@ chosen_method = METHODS[chosen_index]
 
 puts "\nВот что получилось:"
 
-case chosen_method
-when :MD5 then puts Digest::MD5.hexdigest(string)
-when :SHA1 then puts Digest::SHA1.hexdigest(string)
-when :SHA2 then puts Digest::SHA2.hexdigest(string)
-end
+output =
+  case chosen_method
+  when :MD5
+    Digest::MD5.hexdigest(string)
+  when :SHA1
+    Digest::SHA1.hexdigest(string)
+  when :SHA2
+    Digest::SHA2.hexdigest(string)
+  end
+
+puts output
